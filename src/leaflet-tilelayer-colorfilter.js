89589edc87c62a8651d36672a4294132f2abf981
Kwa Linux <claudio.tk93@gmail.com>
2025-08-31 11:33:48 -0300

    chore: use side effect strategy

--- src/leaflet-tilelayer-colorfilter.js
+++ src/leaflet-tilelayer-colorfilter.js
@@ -7,51 +7,53 @@
 
 import { TileLayer } from 'leaflet';
 
-export class TileLayerColorFilter extends TileLayer {
-    constructor(url, options) {
-        super(url, options);
-    }
+const colorFilter = function () {
+    let VALIDFILTERS = [
+        'blur:px',
+        'brightness:%', 'bright:brightness:%', 'bri:brightness:%',
+        'contrast:%', 'con:contrast:%',
+        'grayscale:%', 'gray:grayscale:%',
+        'hue-rotate:deg', 'hue:hue-rotate:deg', 'hue-rotation:hue-rotate:deg',
+        'invert:%', 'inv:invert:%',
+        'opacity:%', 'op:opacity:%',
+        'saturate:%', 'saturation:saturate:%', 'sat:saturate:%',
+        'sepia:%', 'sep:sepia:%',
+    ]
 
-    colorFilter() {
-        let VALIDFILTERS = [
-            'blur:px',
-            'brightness:%', 'bright:brightness:%', 'bri:brightness:%',
-            'contrast:%', 'con:contrast:%',
-            'grayscale:%', 'gray:grayscale:%',
-            'hue-rotate:deg', 'hue:hue-rotate:deg', 'hue-rotation:hue-rotate:deg',
-            'invert:%', 'inv:invert:%',
-            'opacity:%', 'op:opacity:%',
-            'saturate:%', 'saturation:saturate:%', 'sat:saturate:%',
-            'sepia:%', 'sep:sepia:%',
-        ]
-
-        let colorFilterOptions = this.options.filter ? this.options.filter : [];
-        let filterSettings = colorFilterOptions.map((opt) => {
-            let filter = opt.toLowerCase().split(':');
-            if (filter.length === 2) {
-                let match = VALIDFILTERS.find(vf => {
-                    return (vf.split(':')[0] === filter[0]);
-                });
-                if (match) {
-                    match = match.split(':');
-                    filter[1] += /^\d+$/.test(filter[1]) ? match[match.length - 1] : ''
-                    return (`${match[match.length - 2]}(${filter[1]})`);
-                }
+    let colorFilterOptions = this.options.colorFilter ? this.options.colorFilter : [];
+    let filterSettings = colorFilterOptions.map((opt) => {
+        let filter = opt.toLowerCase().split(':');
+        if (filter.length === 2) {
+            let match = VALIDFILTERS.find(vf => {
+                return (vf.split(':')[0] === filter[0]);
+            });
+            if (match) {
+                match = match.split(':');
+                filter[1] += /^\d+$/.test(filter[1]) ? match[match.length - 1] : ''
+                return (`${match[match.length - 2]}(${filter[1]})`);
             }
-            return ('');
-        }).join(' ');
-        return (filterSettings);
-    }
+        }
+        return ('');
+    }).join(' ');
+    return (filterSettings);
+};
 
-    onAdd(map) {
-        super.onAdd(map);
-        this._container.style.filter = this.colorFilter();
+const updateColorFilter = function (newFilter) {
+    this.options.colorFilter = newFilter;
+    if (this._container) {
+        this._container.style.filter = this._colorFilter();
     }
+};
 
-    updateFilter(newFilter) {
-        this.options.filter = newFilter;
-        if (this._container) {
-            this._container.style.filter = this.colorFilter();
+const originalOnAdd = TileLayer.prototype.onAdd;
+
+TileLayer.include({
+    _colorFilter: colorFilter,
+    updateColorFilter: updateColorFilter,
+    onAdd: function(map) {
+        originalOnAdd.call(this, map);
+        if (this.options.colorFilter) {
+            this._container.style.filter = this._colorFilter();
         }
     }
-}
+});
